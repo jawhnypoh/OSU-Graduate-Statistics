bc326d4fedb89c7e2f09e40e08e7a5eedd9790c7
jawhnypoh <po.johnny@gmail.com>
2018-06-05 17:47:56 -0700

    Updating main page to have total graduates statistics JS

---
+++ js/totalchart.js
@@ -0,0 +1,36 @@
+
+Highcharts.chart('total-container', {
+  chart: {
+    type: 'bar'
+  },
+  title: {
+    text: ''
+  },
+  xAxis: {
+    categories: ['2001', '2005', '2009', '2013', '2017']
+  },
+  yAxis: {
+    min: 0,
+    title: {
+      text: 'Total fruit consumption'
+    }
+  },
+  legend: {
+    reversed: true
+  },
+  plotOptions: {
+    series: {
+      stacking: 'normal'
+    }
+  },
+  series: [{
+    name: 'Doctorate',
+    data: [202,	255, 304, 353, 396]
+  }, {
+    name: 'Master',
+    data: [584, 770, 642, 735, 984]
+  }, {
+    name: 'Bachelor',
+    data: [2531, 3058, 3127, 3913, 5148]
+  }]
+});
